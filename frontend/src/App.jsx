f8a12031cf621553a4c015accdbe35e9050256bc
ujjalmukthakim <ujjalahammed2143@gmail.com>
2026-02-05 01:30:02 +0600

    crud done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form';
+
 import api from './api/api'
 
 
@@ -21,13 +23,54 @@ useEffect(()=>{
   getStudentInfo()
 },[]);
 
+const {register,handleSubmit,formState:{errors},reset}=useForm();
+
+const formSubmit=async(data)=>{
+  await api.post('',data)
+  reset()
+  getStudentInfo()
+
+  
+
+}
+
+const hanldeDelete=async(id)=>{
+  await api.delete(`/${id}/`);
+  getStudentInfo()
+
+
+}
 
   return (
 
+
+    <div>
+      {/* this is input section  */}
+      <form onSubmit={handleSubmit(formSubmit)}>
+        <input placeholder='Name' {...register('name')}/>
+        <input placeholder='Department' {...register('department')}/>
+        <button type='submit'> Add</button>
+      </form>
+
+
+
+
+    {/* this is information section */}
     <div>
       {students.map((student)=>(
-        <p key={student.id}>{student.name}</p>
+        <div>
+
+          <p key={student.id}>{student.name}
+             
+          </p>
+          <p key={student.id}>{student.department}</p>
+             <button>EDIT</button>
+             <button onClick={()=>hanldeDelete(student.id)}>Delete</button>
+
+        </div>
+      
       ))}
+    </div>
     </div>
  
   )
