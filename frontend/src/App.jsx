90a17c6eb3efbf4aed2032d2f47d13b51d5a6806
ujjalmukthakim <ujjalahammed2143@gmail.com>
2026-02-08 22:56:08 +0600

    basic design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -73,12 +73,27 @@ const handleEdit=(student)=>{
 
 
     <div>
+      <div>
+        {/* header Section */}
+        <div>
+          <h1 className='text-gray-800 font-extrabold text-3xl tracking-tight'>Student Management</h1>
+          <p className='mt-2 text-gray-900'>Managing Department Records</p>
+
+        </div>
+
       {/* this is input section  */}
       <form onSubmit={handleSubmit(formSubmit)}>
-        <input placeholder='Name' {...register('name')}/>
-        <input placeholder='Department' {...register('department')}/>
-        <button type='submit'> {edit?'Update':'Add'}</button>
+        <input placeholder='Name' {...register('name')}
+        className='border border-gray-300 rounded-lg px-4 py-2.5 focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition-all'
+        />
+        <input placeholder='Department' {...register('department')}
+        className='border border-gray-300 rounded-lg px-4 py-2.5 focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition-all'
+        />
+        <button type='submit'
+        className={`text-white px-6 py-2.5 rounded-lg font-medium transition-colors ${edit?'bg-amber-500 hover:bg-amber-700':'bg-blue-500 hover:bg-blue-700'}`}
+        > {edit?'Update':'Add Student'}</button>
       </form>
+      </div>
 
 
 
@@ -92,8 +107,12 @@ const handleEdit=(student)=>{
              
           </p>
           <p key={student.id}>{student.department}</p>
-             <button onClick={()=>handleEdit(student)}>EDIT</button>
-             <button onClick={()=>hanldeDelete(student.id)}>Delete</button>
+             <button onClick={()=>handleEdit(student)}
+              className='bg-blue-50 text-blue-700 rounded-md px-3 py-1.5 text-sm font-medium hover:bg-blue-100 transition-colors'
+              >EDIT</button>
+             <button onClick={()=>hanldeDelete(student.id)}
+              className='bg-red-50 text-red-700 rounded-md px-3 py-1.5 text-sm font-medium hover:bg-red-100 transition-colors'
+              >Delete</button>
 
         </div>
       
